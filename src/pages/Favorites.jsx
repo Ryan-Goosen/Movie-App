b52ad50f2e10d5031bf0eed51f1db66d6d6315b7
Ryan Goosen <127750479+Ryan-Goosen@users.noreply.github.com>
2025-12-24 13:39:06 +0200

    adding project to repo

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+import '../css/Favorites.css'
+
+function Favorite(){
+    return <div className="favorites-empty">
+        <h2>No favorite Movies yet</h2>
+        <p>Start adding movies to your favrioutes and they will apear here.</p>
+    </div>
+}
+
+export default Favorite
