487fd379014a60cf4cf33043e71b59fc07aae570
Ryan Goosen <127750479+Ryan-Goosen@users.noreply.github.com>
2025-12-29 15:00:54 +0200

    test of login

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -5,3 +5,5 @@ const DB_API_KEY = import.meta.env.VITE_APP_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 console.log(DB_API_KEY)
 console.log(BASE_DB_URL)
 export const supabase = createClient(BASE_DB_URL, DB_API_KEY);
+
+// # TEST
